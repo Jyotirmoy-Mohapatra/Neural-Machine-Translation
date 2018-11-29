a7b6f09c82ba5e3b36056093ad28f457005869ed
Jyotirmoy-Mohapatra <jm7432@nyu.edu>
2018-11-29 18:49:38 -0500

    resolve conflict

--- utils/train.py
+++ utils/train.py
@@ -1,13 +1,46 @@
 import time
 import math
 from utils.plot import *
-from utils.data import *
-
+#from utils.data import *
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+from Transformer import *
+#from utils.Data_Loader_Transformer import *
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device=torch.device("cpu")
+
 
 #teacher_forcing_ratio = 0.5
+def run_epoch(args,data_iter, model, loss_compute):
+    "Standard Training and Logging Function"
+    start = time.time()
+    total_tokens = 0
+    total_loss = 0
+    tokens = 0
+    for i, batch in enumerate(data_iter):
+        batch.src=batch.src.to(device)
+        batch.trg=batch.trg.to(device)
+        batch.trg_mask=batch.trg_mask.to(device)
+        batch.src_mask=batch.src_mask.to(device)
+        batch.trg_y=batch.trg_y.float().to(device)
+        batch.ntokens=batch.ntokens.float().to(device)
+        #print(time.time()-start_time)
+        out = model.forward(batch.src, batch.trg, 
+                            batch.src_mask, batch.trg_mask)
+        start_time=time.time()
+        loss = loss_compute(out, batch.trg_y, batch.ntokens)
+        total_loss += loss
+        total_tokens += batch.ntokens
+        tokens += batch.ntokens
+        if i % 50 == 1:
+            elapsed = time.time() - start
+            print("Epoch Step: %d Loss: %f Tokens per Sec: %f" %
+                    (i, loss / batch.ntokens, tokens / elapsed))
+            start = time.time()
+            tokens = 0
+    return total_loss / total_tokens
+
 
 
 def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
@@ -78,7 +111,7 @@ def timeSince(since, percent):
 
 
 
-def trainIters(args, input_lang, output_lang, pairs, encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.001):
+def trainIters(args, input_lang, output_lang, pairs, encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.001, transformer=False):
     start = time.time()
     plot_losses = []
     print_loss_total = 0  # Reset every print_every
@@ -95,17 +128,14 @@ def trainIters(args, input_lang, output_lang, pairs, encoder, decoder, n_iters, 
         training_pair = training_pairs[iter - 1]
         input_tensor = training_pair[0]
         target_tensor = training_pair[1]
-
-        loss = train(input_tensor, target_tensor, encoder,
-                     decoder, encoder_optimizer, decoder_optimizer, criterion)
+        loss = train(input_tensor, target_tensor, encoder,decoder, encoder_optimizer, decoder_optimizer, criterion)
         print_loss_total += loss
         plot_loss_total += loss
-
         if iter % print_every == 0:
             torch.save(encoder.state_dict(), scratch+args.output+"encoder.pth")
             torch.save(decoder.state_dict(), scratch+args.output+"decoder.pth")
             print_loss_avg = print_loss_total / print_every
-            print_loss_total = 0
+            print_loss_total = -1
             print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
                                          iter, iter / n_iters * 100, print_loss_avg))
 
@@ -115,3 +145,40 @@ def trainIters(args, input_lang, output_lang, pairs, encoder, decoder, n_iters, 
             plot_loss_total = 0
 
     showPlot(args, plot_losses)
+
+def trainItersTransformer(args,model,train_iter,valid_iter,criterion, pad_idx, n_iters=1,plot_every=10,save_every=10):
+    plot_losses = 0
+    train_plot_losses=0
+    plot_loss_list=[]
+    train_plot_loss_list=[]
+    model_opt = NoamOpt(model.src_embed[0].d_model, 1, 200,
+            torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+    for epoch in range(1,n_iters+1):
+        print("\nEpoch number:",epoch)
+        #start_time_train=time.time()
+        model.train()
+        train_loss=run_epoch(args,(rebatch(pad_idx, b) for b in train_iter), 
+                  model, 
+                  SimpleLossCompute(model.generator, criterion, opt=model_opt))
+        #print("Time to train:",epoch,time.time()-start_time_train)
+        #start_time_val=time.time()
+
+        model.eval()
+        loss = run_epoch(args,(rebatch(pad_idx, b) for b in valid_iter), 
+                          model, 
+                          SimpleLossCompute(model.generator, criterion, opt=None))
+        #print("Time to validate:",epoch,time.time()-start_time_val)
+        plot_losses+=loss
+        train_plot_losses+=train_loss
+        if epoch % save_every==0:
+            print("... Saving model.")
+            torch.save(model.state_dict(), scratch+args.output+str(epoch)+"_transformer_model.pth")
+        if epoch % plot_every==0:
+            plot_loss_avg=plot_losses/plot_every
+            train_plot_loss_avg=train_plot_losses/plot_every
+            plot_loss_list.append(plot_loss_avg)
+            train_plot_loss_list.append(train_plot_loss_avg)
+            plot_losses=0
+            train_plot_losses=0
+    showPlot(args, plot_loss_list)
+    showPlot(args,train_plot_loss_list,train=True)
