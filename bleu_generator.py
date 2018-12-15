f2c3c8de5ed92481c219c7d3e5d71c2c9815c479
Shubhankar Panse <shubhankarpanse1995@gmail.com>
2018-12-14 21:16:29 -0500

    Update bleu_generator.py

--- bleu_generator.py
+++ bleu_generator.py
@@ -5,6 +5,7 @@ try:
 	parser = argparse.ArgumentParser(description='Bleu score generator')
 	parser.add_argument('--input', type=str, default='', metavar='P',
                     help="File name to get bleu score")
+	parser.add_argument('--bleu',type=str,default="nltk",metavar='P',help="Which bleu score to use")
 	args = parser.parse_args()
 	in_file=open(args.input,'r')
 except:
@@ -15,16 +16,27 @@ else:
 	in_string=in_file.read()
 	##nltk bleu takes tokenized complete string and not sentences
 	in_list=in_string.split("\n")
-	for line in in_list:
-		if line.startswith("Translation: "):
+	if args.bleu=="nltk":
+		for line in in_list:
+			if line.startswith("Translation: "):
 			#system_sentences.append(line[13:])
-			system_sentences+=line[13:].split(" ")
-		elif line.startswith("Target: "):
+				system_sentences+=line[13:].split(" ")
+			elif line.startswith("Target: "):
 			#ref_sentences.append(line[8:])
-			ref_sentences+=line[8:].split(" ")
+				ref_sentences+=line[8:].split(" ")
+	else:
+		for line in in_list:
+			if line.startswith("Translation: "):
+				system_sentences.append(line[13:])
+				#system_sentences+=line[13:].split(" ")
+			elif line.startswith("Target: "):
+				ref_sentences.append(line[8:])
+				#ref_sentences+=line[8:].split(" ")
 	ref_stream=[ref_sentences]
 	#print(len(system_sentences),len(ref_stream[0]))
 	#print(ref_stream)
-	print("NLTK bleu score is:",bleu_score.corpus_bleu([ref_stream],[system_sentences]))	
-	#print(corpus_bleu(system_sentences,ref_stream,tokenize="split_tokenizer"))
+	if args.bleu=="nltk":
+		print("NLTK bleu score is:",bleu_score.corpus_bleu([ref_stream],[system_sentences]))	
+	else:
+		print(corpus_bleu(system_sentences,ref_stream,tokenize="13a"))
 	
