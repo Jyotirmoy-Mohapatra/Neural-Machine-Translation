43ed6c35bf55ae995515abba7f79284a04c5a0cb
Jyotirmoy Mohapatra <38120608+Jyotirmoy-Mohapatra@users.noreply.github.com>
2018-12-14 21:23:35 -0500

    Update params.py

--- params.py
+++ params.py
@@ -3,14 +3,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 SOS_token = 0
 EOS_token = 1
-MAX_LENGTH = 5
+MAX_LENGTH = 100
 
 hidden_size = 256
 
-no_of_iterations = 10000
+no_of_iterations = 20
 
 teacher_forcing_ratio = 0.5
 
-scratch = "/home/jyotirmoy/Desktop/NLP Project/"
+scratch = ""
 
 home = ""
